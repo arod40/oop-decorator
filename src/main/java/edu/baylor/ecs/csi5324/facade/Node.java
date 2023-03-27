fb9a0a7733c21ebe88878927b7029f8113e8829d
arod40 <arodacademic@gmail.com>
2023-03-27 00:06:12 -0500

    First commit

---
+++ src/main/java/edu/baylor/ecs/csi5324/facade/Node.java
@@ -0,0 +1,8 @@
+package edu.baylor.ecs.csi5324.facade;
+/**
+ * Node, represents Node in Abstract Syntax Tree.
+ *
+ */
+public class Node {
+	 
+}
---
+++ src/main/java/edu/baylor/ecs/csi5324/facade/OperandNode.java
@@ -0,0 +1,17 @@
+package edu.baylor.ecs.csi5324.facade;
+/**
+ * OperandNode, represents OperandNode in Abstract Syntax Tree.
+ *
+ */
+public class OperandNode extends Node {
+
+  private int value;
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+}
